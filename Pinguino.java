f4dc9349a0580d45f5df95844e6b6f068fac9dbd
JavierGgithub <93383118+campusJavierG@users.noreply.github.com>
2021-12-15 16:57:28 +0100

    Create Pinguino.java

---
+++ Pinguino.java
@@ -0,0 +1,10 @@
+
+public class Pinguino extends Animal {//extends para heredar
+	//se sobreescribe el m�todo heredado
+	public void correr() {
+		System.out.println("estoy correteando");
+	
+	}
+	
+
+}
