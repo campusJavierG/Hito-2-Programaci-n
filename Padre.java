7b19c2ab5d174ae9d7cde173dba69d455eb5359c
JavierGgithub <93383118+campusJavierG@users.noreply.github.com>
2021-12-15 16:55:57 +0100

    Create Padre.java

---
+++ Padre.java
@@ -0,0 +1,13 @@
+
+public abstract class Padre {//clase abstracta para que no pueda ser instanciada
+	private String texto;
+	private int numero;
+	public Padre() {
+		// TODO Auto-generated constructor stub
+	}
+	//se crea el m�todo saludar
+	public void Saludar() {
+		System.out.println("Hola, soy el padre");
+		}
+		
+	}
