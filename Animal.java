6c1c6b3047aed63fc18cb9f9364207c3ed9b0e50
JavierGgithub <93383118+campusJavierG@users.noreply.github.com>
2021-12-15 16:56:51 +0100

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,12 @@
+
+public class Animal {
+	 public void correr() {
+			System.out.println("estoy corriendo");
+	 }
+	 public Animal() {}
+	public static void calurosos(int i) {
+		// TODO Auto-generated method stub
+		System.out.println("estoy corriendo");
+	}
+	
+}
