6ae848a2c0f831ab4b370009b57b4025173b9236
JavierGgithub <93383118+campusJavierG@users.noreply.github.com>
2021-12-15 16:56:24 +0100

    Create Hija.java

---
+++ Hija.java
@@ -0,0 +1,11 @@
+// se utiliza extends para hacer la herencia
+public class Hija extends Padre {
+//al cambiar el m�todo saludar se produce la sobreescritura
+	public void Saludar() {
+		System.out.println("Hola,soy la hija");
+		
+	}
+	public Hija() {
+		// TODO Auto-generated constructor stub
+	}
+}
